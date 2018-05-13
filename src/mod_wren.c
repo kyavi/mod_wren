b32830c4d4873efdf5a81484edb0df602bed3b50
kyavi <talk@kyavi.xyz>
2018-05-13 18:20:51 +0100

    Adds initial embedded wren parsing
    Parsing a mixed Wren/HTML page, printing out all of the HTML as it's
    written and passing on the Wren to be interpreted as code. Hopefully
    escapes out everything that should be escaped so it runs correctly, but
    at this point, who knows?
    
    Currently allocates a stupidly large buffer for the output code - this
    will be changed later to be a bit more solid.

--- src/mod_wren.c
+++ src/mod_wren.c
@@ -9,6 +9,9 @@
 
 #include "wren.h"
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
 /**
  * A WrenState contains a VM and everything relevant to the current request
  * it's serving.
@@ -29,6 +32,11 @@ WrenState *wren_states;
 
 #define ERROR_END \
 	"</div>"
+
+#define OPENING_TAG "<?wren"
+#define OPENING_TAG_LEN strlen(OPENING_TAG)
+#define CLOSING_TAG "?>"
+#define CLOSING_TAG_LEN strlen(CLOSING_TAG)
 
 /**
  * Sets the output of Wren's print functions.
@@ -172,11 +180,73 @@ static void wren_release_state(WrenState *wren_state)
 	wren_state->lock = false;
 }
 
-static char* wren_load(WrenState *wren_state)
+/**
+ * Write an HTML block inside the file_buf, starting from file_index,
+ * to the output wren_buf, starting at wren_index.
+ *
+ * file_index will get progressed by html_len, the amount of raw HTML
+ * within the file. wren_index will get progressed a bit more, html_len plus
+ * extra characters for function calls and escaping.
+ */
+static void wren_parse_insert_html(char *file_buf, char *wren_buf,
+		size_t *file_index, size_t *wren_index, size_t html_len)
 {
+	if(html_len == 0 || (html_len == 1 && *(file_buf + *file_index) == '\n'))
+		return;
+
+	/* Start write */
+	strncpy(wren_buf + *wren_index, "\nSystem.write(\"", 15);
+	*wren_index += 15;
+
+	/*
+	 * Write the actual HTML segment to the buffer, escaping forbidden
+	 * characters as we go.
+	 *
+	 * This isn't the most elegantly written thing in the world.
+	 */
+	char *next_quote   = strchr(file_buf + *file_index, '"')  ?: (char*)INTPTR_MAX;
+	char *next_percent = strchr(file_buf + *file_index, '%')  ?: (char*)INTPTR_MAX;
+	char *next_slash   = strchr(file_buf + *file_index, '\\') ?: (char*)INTPTR_MAX;
+
+	do {
+		char *next_char = MIN(next_quote, MIN(next_percent, next_slash));
+
+		if(next_char > file_buf + (*file_index) + html_len)
+			break;
+
+		size_t segment_len = next_char - (file_buf + *file_index);
+
+		strncpy(wren_buf + *wren_index, (file_buf + *file_index), segment_len);
+		*file_index += segment_len;
+		*wren_index += segment_len + 1;
+		wren_buf[*wren_index - 1] = '\\';
+		html_len -= segment_len;
+
+		next_quote   = strchr(file_buf + *file_index + 1, '"')  ?: (char*)INTPTR_MAX;
+		next_percent = strchr(file_buf + *file_index + 1, '%')  ?: (char*)INTPTR_MAX;
+		next_slash   = strchr(file_buf + *file_index + 1, '\\') ?: (char*)INTPTR_MAX;
+	} while(html_len > 0);
+
+	strncpy(wren_buf + *wren_index, (file_buf + *file_index), html_len);
+	*file_index += html_len;
+	*wren_index += html_len;
+
+	/* Close up the write. */
+	strncpy(wren_buf + *wren_index, "\")\n", 3);
+	*wren_index += 3;
+}
+
+/**
+ * Parse an Wren page, looking for blocks between OPENING_TAG and CLOSING_TAG to
+ * run as regular Wren code, and wrapping the rest (HTML blocks) inside
+ * System.write calls for Wren to spit out.
+ */
+static char* wren_parse(WrenState *wren_state)
+{
+	/* Open up a file and write it to a buffer we can work from. */
 	FILE *file = fopen(wren_state->request_rec->filename, "r");
-	char *buf;
-	size_t file_len;
+	char *file_buf, *out_buf;
+	size_t file_len, read_len;
 
 	if(file == NULL)
 		return NULL;
@@ -185,26 +255,100 @@ static char* wren_load(WrenState *wren_state)
 	file_len = ftell(file) ?: 1;
 	fseek(file, 0, SEEK_SET);
 
-	buf = calloc(file_len + 1, 1);
-	file_len = fread(buf, 1, file_len, file);
-	buf[file_len + 1] = '\0';
+	file_buf = calloc(file_len + 1, 1);
+	read_len = fread(file_buf, 1, file_len, file);
 	fclose(file);
 
-	return buf;
+	if(read_len != file_len) {
+		free(file_buf);
+		return NULL;
+	}
+
+	/*
+	 * Allocate another buffer for the output Wren code. This needs to be
+	 * bigger than the input to account for the function calls and escaping
+	 * we add.
+	 *
+	 * TODO: There needs to be some method of dynamically resizing this, since
+	 * it's currently possible to exceed it.
+	 */
+	out_buf = calloc(file_len * 15, 1);
+
+	size_t file_index = 0;
+	size_t out_index = 0;
+	char *next_tag;
+
+	/*
+	 * Go through the file looking for Wren tags and converting HTML blocks to
+	 * Wren System.write statements.
+	 */
+	while(file_index < file_len) {
+		next_tag = strstr((char*)(file_buf + file_index), OPENING_TAG);
+
+		/*
+		 * There are no more Wren segments to parse. Add the rest to the output
+		 * buffer as HTML.
+		 */
+		if(next_tag == NULL) {
+			size_t html_len = file_len - file_index;
+			wren_parse_insert_html(file_buf, out_buf,
+					&file_index, &out_index, html_len);
+			break;
+		}
+
+		/*
+		 * Otherwise, we've hit a Wren segment. Find the end tag and place the
+		 * code in out output buffer.
+		 */
+		if(file_len - file_index > 0) {
+			size_t html_len = next_tag - (file_buf + file_index);
+			wren_parse_insert_html(file_buf, out_buf,
+					&file_index, &out_index, html_len);
+		}
+
+		file_index += OPENING_TAG_LEN;
+		char *closing_tag = strstr(file_buf + file_index, CLOSING_TAG);
+
+		if(closing_tag == NULL) {
+			/*
+			 * Mismatched opening/closing tag. This should probably be handled,
+			 * but for now, just let wren fail.
+			 */
+			break;
+		}
+
+		strncpy(out_buf + out_index, file_buf + file_index, closing_tag - (file_buf + file_index));
+		out_index += closing_tag - (file_buf + file_index);
+		file_index += closing_tag - (file_buf + file_index) + CLOSING_TAG_LEN;
+	}
+
+	free(file_buf);
+	return out_buf;
 }
 
+/**
+ * Main Wren handler that gets hooked when we call a Wren file, and converts
+ * the file to something that can be understood by the WrenVM and runs it.
+ */
 static int wren_handler(request_rec *r)
 {
 	WrenState *wren_state = wren_acquire_state(r);
 	char *wren_code;
 
-	if((wren_code = wren_load(wren_state)) == NULL) {
+	if((wren_code = wren_parse(wren_state)) == NULL) {
 		wren_release_state(wren_state);
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 
-	ap_set_content_type(r, "text/html");
-	wrenInterpret(wren_state->vm, wren_code);
+	/* Pre-release debug: ?raw will print the generated Wren code. */
+	if(strcmp(r->args ?: "", "compiled") == 0) {
+		ap_set_content_type(r, "text/plain");
+		ap_rprintf(r, "%s", wren_code);
+	}
+	else {
+		ap_set_content_type(r, "text/html");
+		wrenInterpret(wren_state->vm, wren_code);
+	}
 
 	wren_release_state(wren_state);
 	free(wren_code);
